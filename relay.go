b610fe3f5457e12b813ded74a04c79fab04d45e2
iovxw <iovxw@outlook.com>
2022-04-28 11:35:51 +0800

    Refactor: be a gopher

--- relay.go
+++ relay.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"io"
 	"log"
 	"net/http"
@@ -15,28 +14,18 @@ func relay(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	var body io.Reader
-	if r.Method == "POST" {
-		body = r.Body
-	}
-
-	req, _ := http.NewRequest(r.Method, wsurl, body)
-	a, err := client.Do(req)
+	req, _ := http.NewRequest(r.Method, wsurl, r.Body)
+	resp, err := client.Do(req)
 	if err != nil {
 		w.WriteHeader(502)
-		fmt.Println(502, err)
+		log.Println(502, err)
 		return
 	}
 
-	for k, _ := range a.Header {
-		w.Header().Set(k, a.Header.Get(k))
+	for k, v := range resp.Header {
+		w.Header()[k] = v
 	}
+	w.WriteHeader(resp.StatusCode)
 
-	// if a.StatusCode != 200 {
-	// 	log.Println(a.StatusCode, reqip, ip2dc(reqip), wsurl)
-	// }
-
-	w.WriteHeader(a.StatusCode)
-
-	io.Copy(w, a.Body)
+	io.Copy(w, resp.Body)
 }
