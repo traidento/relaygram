0ad0a525ac9bb62d0e4c804059b664b384741556
iovxw <iovxw@outlook.com>
2022-04-28 17:27:13 +0800

    Set timeout
    The default timeout of Telegram is 25 seconds

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ var client *http.Client
 
 func main() {
 	listen := flag.String("l", "127.0.0.1:26641", "HttpProxy listen port")
-	_nekoXProxyString := flag.String("p", "", "NekoX Proxy URL (keep empty if you don't know)")
+	_nekoXProxyString := flag.String("p", "", "NekoX Proxy URL")
 	flag.Parse()
 
 	var ok bool
@@ -26,16 +26,19 @@ func main() {
 	}
 
 	if !ok {
-		log.Println("Failed to parse NekoX proxy.")
+		log.Println("NekoX Proxy URL is required")
 		return
 	}
 
-	client = &http.Client{}
+	client = &http.Client{
+		Timeout: 25 * 2 * time.Second,
+	}
 
 	http.HandleFunc("/", relay)
 	server := &http.Server{
 		Addr:         *listen,
-		WriteTimeout: 10 * time.Second,
+		WriteTimeout: 25 * 2 * time.Second,
+		ReadTimeout: 25 * 2 * time.Second,
 	}
 
 	log.Println("Telegram HTTP Proxy started at", *listen)
